d517d2c5d0c085fa0cbc83f12c02e6ecaee0ac9b
Alan Nix <alanneilnix@gmail.com>
2025-10-29 15:13:01 -0400

    feat: allow city specification in searches

--- gamechanger_client/endpoints/search.py
+++ gamechanger_client/endpoints/search.py
@@ -8,5 +8,5 @@ class SearchEndpoint(RestEndpoint):
     def __init__(self, session):
         super().__init__(session, 'search')
 
-    def search(self, name, types=[], seasons=[], sport=None, states=[], page=0):
-        return super().post(name=name, types=types, seasons=seasons, sport=sport, states=states)
+    def search(self, name, types=[], seasons=[], sport=None, city=None, states=[], page=0):
+        return super().post(name=name, types=types, seasons=seasons, sport=sport, city=city, states=states)
